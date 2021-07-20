c93c9ee5955be3acf6c2c6d0d179a3081385896e
gleb5783 <gleb5783@mail.ru>
2021-07-20 16:01:47 +0300

    asdasd

--- js/main.js
+++ js/main.js
@@ -1,38 +1,51 @@
-let select = function () {
-  let selectHeader = document.querySelectorAll('.main-form-order__select__select-header');
-  let selectItem = document.querySelectorAll('.main-form-order__select-item');
+'use strict';
+var ESC_BUTTON = 'Esc';
+var ESCAPE_BUTTON = 'Escape';
+var popup = document.querySelector('.popup');
+var closePopupButton = document.querySelector('.popup__close-btn');
+var openPopup = document.querySelector('.header__list-contacts-btn');
 
-  selectHeader.forEach(item => {
-      item.addEventListener('click', selectToggle)
-  });
-
-  selectItem.forEach(item => {
-      item.addEventListener('click', selectChoose)
-  });
-
-  function selectToggle() {
-      this.parentElement.classList.toggle('is-active');
-  }
-
-  function selectChoose() {
-      let text = this.innerText,
-          select = this.closest('.main-form-order__select'),
-          currentText = select.querySelector('.main-form-order__select-current');
-      currentText.innerText = text;
-      select.classList.remove('is-active');
-
-  }
-
-};
-
-
-select();
-
-let slider = document.querySelector('#range');
-let sliderValue = document.querySelector('#value');
-
-sliderValue.innerText = slider.value + " %";
-
-slider.oninput = function() {
-  sliderValue.innerText = this.value + " %";
+function isEscEvent(evt) {
+  return evt.key === ESCAPE_BUTTON || evt.key === ESC_BUTTON;
 }
+
+function closeEscMenu(evt) {
+  if (isEscEvent(evt)) {
+    popup.classList.add('popup--hidden');
+  }
+}
+
+function closeClickPopup(evt) {
+  if(evt.target === popup) {
+    popup.classList.add('popup--hidden');
+  }
+}
+
+var closePopup = () => {
+  document.removeEventListener('click', closeClickPopup);
+  document.removeEventListener('keydown', closeEscMenu);
+  popup.classList.add('popup--hidden');
+}
+
+var showPopup = () => {
+  document.getElementById('popup-name').focus();
+  document.addEventListener('click', closeClickPopup);
+  document.addEventListener('keydown', closeEscMenu);
+  popup.classList.remove('popup--hidden');
+  document.querySelector('#popup-name').focus();
+}
+
+openPopup.addEventListener('click', showPopup);
+closePopupButton.addEventListener('click', closePopup);
+
+var element = document.getElementById('tel');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(element, maskOptions);
+
+var elements = document.getElementById('popup-tel');
+var maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+var mask = IMask(elements, maskOptions);
