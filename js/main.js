97a3ed3da91978ff6c634409a9ea07891734c18e
gleb5783 <gleb5783@mail.ru>
2021-07-21 18:37:47 +0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-'use strict';
 var ESC_BUTTON = 'Esc';
 var ESCAPE_BUTTON = 'Escape';
 var popup = document.querySelector('.popup');
@@ -42,10 +41,69 @@ var element = document.getElementById('tel');
 var maskOptions = {
   mask: '+{7}(000)000-00-00'
 };
-var mask = IMask(element, maskOptions);
+var mask1 = IMask(element, maskOptions);
 
 var elements = document.getElementById('popup-tel');
 var maskOptions = {
   mask: '+{7}(000)000-00-00'
 };
+
 var mask = IMask(elements, maskOptions);
+
+var anchor = document.querySelector('.main-content__scrool-link');
+var anchor2 = document.querySelector('.main-content__btn');
+
+anchor.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  var blockID = anchor.getAttribute('href').substr(1);
+
+  document.getElementById(blockID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
+
+anchor2.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  var blockID = anchor.getAttribute('href').substr(1);
+
+  document.getElementById(blockID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
+
+var btn = document.querySelector('.footer__list-nav-open');
+var dropDown = document.querySelector('.footer__list-nav-item-bottom');
+var btn2 = document.querySelector('.footer__list-address-open');
+var dropDown2 = document.querySelector('.footer__list-address-item-bottom');
+
+if(document.documentElement.scrollWidth < 768) {
+  dropDown.classList.add('footer__list-nav-item--hidden');
+  dropDown2.classList.add('footer__list-nav-item--hidden');
+  btn.classList.remove('footer__list-nav-close');
+  btn2.classList.remove('footer__list-address-close');
+}
+
+function showDropDown() {
+  if(dropDown2.classList.length === 1) {
+    btn2.classList.toggle('footer__list-address-close');
+    dropDown2.classList.toggle('footer__list-nav-item--hidden');
+  }
+  btn.classList.toggle('footer__list-nav-close');
+  dropDown.classList.toggle('footer__list-nav-item--hidden');
+}
+
+function showDropDown2() {
+  if(dropDown.classList.length === 1) {
+    btn.classList.toggle('footer__list-nav-close');
+    dropDown.classList.toggle('footer__list-nav-item--hidden');
+  }
+  btn2.classList.toggle('footer__list-address-close');
+  dropDown2.classList.toggle('footer__list-nav-item--hidden');
+}
+
+btn.addEventListener('click', showDropDown);
+btn2.addEventListener('click', showDropDown2);
