535337a3cd5f0380047f3be0fef59cd61b0f1cea
gleb5783 <gleb5783@mail.ru>
2021-10-04 14:25:11 +0300

    a6

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+const cardTemplate = document.querySelector('#card').content.querySelector('.card__item');
+const cardList = document.querySelector('.card__list');
+const addBtn = document.querySelector('.card__load-more-btn');
+
+const fragment = document.createDocumentFragment();
+
+const number = () => {
+    return Math.round(Math.random() * (101 - 0) + 0);
+}
+
+function addNewCard () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function(response) {
+      return response.json()
+    })
+    .then((response) => {
+        for (let i = 1; i < 6; i++) {
+            const cardElement = cardTemplate.cloneNode(true);
+            cardElement.querySelector('h4').textContent = response[number()].title;
+            cardElement.querySelector('p').textContent = response[number()].body;
+            fragment.appendChild(cardElement);
+        }
+    cardList.appendChild(fragment);
+  });
+}
+
+addBtn.addEventListener('click', addNewCard);
 $(document).ready(function(){
     $.fn.setCursorPosition = function(pos) {
         if ($(this).get(0).setSelectionRange) {
@@ -34,7 +61,7 @@ const ESCAPE_BUTTON = 'Escape';
 const popup = document.querySelector('.popup');
 const body = document.querySelector('.body');
 const closePopupBtn = document.querySelector('.popup__close-btn');
-const applicationBtn = document.querySelectorAll('#application');
+const applicationBtn = document.querySelectorAll('.btn-application');
 
 function tabIndexOn () {
     document.querySelectorAll('*').forEach((el) => el.setAttribute('tabindex', '-1'));
